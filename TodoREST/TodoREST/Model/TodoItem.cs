8b4e3f88c4e9a281379894599f9eac6dd1d82061
Harold Arias <harold_joseph_07@hotmail.com>
2021-06-20 05:41:26 -0500

    first commit

---
+++ TodoREST/TodoREST/Model/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoREST.Model
+{
+    public class TodoItem
+    {
+        public String ID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Notes { get; set; }
+        
+        public String Done { get; set; }
+
+    }
+}
---
+++ TodoREST/TodoREST/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TodoREST.Model;
+
+namespace TodoREST.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoItemPage : ContentPage
+    {
+        bool isNewItem;
+
+        public TodoItemPage(bool isNew = false)
+        {
+            InitializeComponent();
+            isNewItem = isNew;
+        }
+
+        async void onSaveButtonClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            await App.TodoManager.SaveTaskAsync(todoItem, isNewItem);
+            await Navigation.PopAsync();
+        }
+
+        async void onDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            await App.TodoManager.DeleteTaskAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void onCancelButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
